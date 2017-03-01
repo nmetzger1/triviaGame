68a1afb9d2d3e9779a2d6801b1d4e1a7e475cc1e
Nick <numbnick2@gmail.com>
2017-02-28 23:21:59 -0500

    added all questions into a single object.  moved the js file

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -2,210 +2,215 @@ window.onload = function () {
     game.readyGame();
 };
 
-//Question / Answers / Images Objects
 var currentQuestion = [];
 
-var q1 = {
-    question: "In what year was Lego founded?",
-    right: "1932",
-    wrong1: "1954",
-    wrong2: "1967",
-    wrong3: "1945",
-    image: "assets/images/originallego.png",
-    picked: false
+//Question Objects
+var questionsObject = {
+
+
+    q1: {
+        question: "In what year was Lego founded?",
+        right: "1932",
+        wrong1: "1954",
+        wrong2: "1967",
+        wrong3: "1945",
+        image: "assets/images/originallego.png",
+        picked: false
+    },
+
+    q2: {
+        question: "This Taj Mahal is the largest Lego set ever produced. <br>  How many bricks did it include?",
+        right: "5,922",
+        wrong1: "3,012",
+        wrong2: "4,821",
+        wrong3: "6,120",
+        image: "assets/images/tajMahal.png",
+        picked: false
+    },
+
+    q3: {
+        question: "Where are the Lego Headquarters located?",
+        right: "Billund, Denmark",
+        wrong1: "Copenhagen, Denmark",
+        wrong2: "Oslo, Norway",
+        wrong3: "Bergen, Norway",
+        image: "assets/images/legohq.png",
+        picked: false
+    },
+
+    q4: {
+        question: "Lego is a derived from the phrase 'leg godt' which means what in English?",
+        right: "Play Well",
+        wrong1: "Plastic Brick",
+        wrong2: "Foot Pain",
+        wrong3: "Many Colors",
+        image: "assets/images/logo.png",
+        picked: false
+    },
+
+    q5: {
+        question: "The first Lego Star Wars sets were released in which year?",
+        right: "1999",
+        wrong1: "1977",
+        wrong2: "1984",
+        wrong3: "2003",
+        image: "assets/images/starwars.png",
+        picked: false
+    },
+
+    q6: {
+        question: "The world record for the tallest lego structure ever built topped out at how many feet?",
+        right: "116",
+        wrong1: "95",
+        wrong2: "160",
+        wrong3: "134",
+        image: "assets/images/bricks.png",
+        picked: false
+    },
+
+    q7: {
+        question: "What is the name of the main protagonist in The Lego Movie?",
+        right: "Emmet",
+        wrong1: "Bob",
+        wrong2: "Benny",
+        wrong3: "Luke",
+        image: "assets/images/emmet.png",
+        picked: false
+    },
+
+    q8: {
+        question: "Approximately how many Lego bricks are produced each year?",
+        right: "19 Billion",
+        wrong1: "50 Billion",
+        wrong2: "7 Billion",
+        wrong3: "100 Billion",
+        image: "assets/images/bricks.png",
+        picked: false
+    },
+
+    q9: {
+        question: "What has NEVER been a theme for Lego sets?",
+        right: "Army",
+        wrong1: "Wild West",
+        wrong2: "Pirates",
+        wrong3: "Space",
+        image: "assets/images/castle.png",
+        picked: false
+    },
+
+    q10: {
+        question: "Of the following, which was NOT an actual Lego Mindstorms fan creation?",
+        right: "VCR",
+        wrong1: "Pancake Printer",
+        wrong2: "Rubik's Cube Solver",
+        wrong3: "Automatic Toilet Flusher",
+        image: "assets/images/mindstorms.png",
+        picked: false
+    },
+
+    q11: {
+        question: "The first LegoLand amusement park opened in which year?",
+        right: "1968",
+        wrong1: "1996",
+        wrong2: "1985",
+        wrong3: "2001",
+        image: "assets/images/legoland.png",
+        picked: false
+    },
+
+    q12: {
+        question: "What is the proper term for a Lego figurine?",
+        right: "Minifigure",
+        wrong1: "Action Figure",
+        wrong2: "Brick Person",
+        wrong3: "Lego Figurine",
+        image: "assets/images/minifigs.png",
+        picked: false
+    },
+
+    q13: {
+        question: "Lego bricks were originally made out of what material?",
+        right: "Wood",
+        wrong1: "Plastic",
+        wrong2: "Plaster",
+        wrong3: "Asbestos",
+        image: "assets/images/bindingbricks.png",
+        picked: false
+    },
+
+    q14: {
+        question: "The most expensive retail set was the Ultimate Collector's Series version of the Millennium Falcon <br> How much did it sell for?",
+        right: "$499",
+        wrong1: "$399",
+        wrong2: "$449",
+        wrong3: "$349",
+        image: "assets/images/falcon.png",
+        picked: false
+    },
+
+    q15: {
+        question: "Which of the following licensed themes has Lego never sold?",
+        right: "Nintendo",
+        wrong1: "Disney Princesses",
+        wrong2: "Harry Potter",
+        wrong3: "Indiana Jones",
+        image: "assets/images/licensed.png",
+        picked: false
+    },
+
+    q16: {
+        question: "Lego started selling their famous building bricks in what year?",
+        right: "1949",
+        wrong1: "1932",
+        wrong2: "1960",
+        wrong3: "1955",
+        image: "assets/images/bricks.png",
+        picked: false
+    },
+
+    q17: {
+        question: "Out of every one million bricks produced, approximately how many fail quality standards?",
+        right: "18",
+        wrong1: "0",
+        wrong2: "110",
+        wrong3: "64",
+        image: "assets/images/bricks.png",
+        picked: false
+    },
+
+    q18: {
+        question: "What is the brand name of Lego products aimed at children 5 and under",
+        right: "Duplo",
+        wrong1: "Lego Jr.",
+        wrong2: "Lil Lego",
+        wrong3: "Fabuland",
+        image: "assets/images/duplo.png",
+        picked: false
+    },
+
+    q19: {
+        question: "What year was the Lego Movie released?",
+        right: "2014",
+        wrong1: "2015",
+        wrong2: "2013",
+        wrong3: "2012",
+        image: "assets/images/legomovie.png",
+        picked: false
+    },
+
+    q20: {
+        question: "Who voices Lego Batman in the Lego series of films?",
+        right: "Will Arnett",
+        wrong1: "Christian Bale",
+        wrong2: "Chris Pratt",
+        wrong3: "Adam West",
+        image: "assets/images/batman.png",
+        picked: false
+    }
 };
 
-var q2 = {
-    question: "This Taj Mahal is the largest Lego set ever produced. <br>  How many bricks did it include?",
-    right: "5,922",
-    wrong1: "3,012",
-    wrong2: "4,821",
-    wrong3: "6,120",
-    image: "assets/images/tajMahal.png",
-    picked: false
-};
-
-var q3 = {
-    question: "Where are the Lego Headquarters located?",
-    right:  "Billund, Denmark",
-    wrong1: "Copenhagen, Denmark",
-    wrong2: "Oslo, Norway",
-    wrong3: "Bergen, Norway",
-    image: "assets/images/legohq.png",
-    picked: false
-};
-
-var q4 = {
-    question: "Lego is a derived from the phrase 'leg godt' which means what in English?",
-    right: "Play Well",
-    wrong1: "Plastic Brick",
-    wrong2: "Foot Pain",
-    wrong3: "Many Colors",
-    image: "assets/images/logo.png",
-    picked: false
-};
-
-var q5 = {
-    question: "The first Lego Star Wars sets were released in which year?",
-    right: "1999",
-    wrong1: "1977",
-    wrong2: "1984",
-    wrong3: "2003",
-    image: "assets/images/starwars.png",
-    picked: false
-};
-
-var q6 = {
-    question: "The world record for the tallest lego structure ever built topped out at how many feet?",
-    right: "116",
-    wrong1: "95",
-    wrong2: "160",
-    wrong3: "134",
-    image: "assets/images/bricks.png",
-    picked: false
-};
-
-var q7 = {
-    question: "What is the name of the main protagonist in The Lego Movie?",
-    right: "Emmet",
-    wrong1: "Bob",
-    wrong2: "Benny",
-    wrong3: "Luke",
-    image: "assets/images/emmet.png",
-    picked: false
-};
-
-var q8 = {
-    question: "Approximately how many Lego bricks are produced each year?",
-    right: "19 Billion",
-    wrong1: "50 Billion",
-    wrong2: "7 Billion",
-    wrong3: "100 Billion",
-    image: "assets/images/bricks.png",
-    picked: false
-};
-
-var q9 = {
-    question: "What has NEVER been a theme for Lego sets?",
-    right: "Army",
-    wrong1: "Wild West",
-    wrong2: "Pirates",
-    wrong3: "Space",
-    image: "assets/images/castle.png",
-    picked: false
-};
-
-var q10 = {
-    question: "Of the following, which was NOT an actual Lego Mindstorms fan creation?",
-    right: "VCR",
-    wrong1: "Pancake Printer",
-    wrong2: "Rubik's Cube Solver",
-    wrong3: "Automatic Toilet Flusher",
-    image: "assets/images/mindstorms.png",
-    picked: false
-};
-
-var q11 = {
-    question: "The first LegoLand amusement park opened in which year?",
-    right: "1968",
-    wrong1: "1996",
-    wrong2: "1985",
-    wrong3: "2001",
-    image: "assets/images/legoland.png",
-    picked: false
-};
-
-var q12 = {
-    question: "What is the proper term for a Lego figurine?",
-    right: "Minifigure",
-    wrong1: "Action Figure",
-    wrong2: "Brick Person",
-    wrong3: "Lego Figurine",
-    image: "assets/images/minifigs.png",
-    picked: false
-};
-
-var q13 = {
-    question: "Lego bricks were originally made out of what material?",
-    right: "Wood",
-    wrong1: "Plastic",
-    wrong2: "Plaster",
-    wrong3: "Asbestos",
-    image: "assets/images/bindingbricks.png",
-    picked: false
-};
-
-var q14 = {
-    question: "The most expensive retail set was the Ultimate Collector's Series version of the Millennium Falcon <br> How much did it sell for?",
-    right: "$499",
-    wrong1: "$399",
-    wrong2: "$449",
-    wrong3: "$349",
-    image: "assets/images/falcon.png",
-    picked: false
-};
-
-var q15 = {
-    question: "Which of the following licensed themes has Lego never sold?",
-    right: "Nintendo",
-    wrong1: "Disney Princesses",
-    wrong2: "Harry Potter",
-    wrong3: "Indiana Jones",
-    image: "assets/images/licensed.png",
-    picked: false
-};
-
-var q16 = {
-    question: "Lego started selling their famous building bricks in what year?",
-    right: "1949",
-    wrong1: "1932",
-    wrong2: "1960",
-    wrong3: "1955",
-    image: "assets/images/bricks.png",
-    picked: false
-};
-
-var q17 = {
-    question: "Out of every one million bricks produced, approximately how many fail quality standards?",
-    right: "18",
-    wrong1: "0",
-    wrong2: "110",
-    wrong3: "64",
-    image: "assets/images/bricks.png",
-    picked: false
-};
-
-var q18 = {
-    question: "What is the brand name of Lego products aimed at children 5 and under",
-    right: "Duplo",
-    wrong1: "Lego Jr.",
-    wrong2: "Lil Lego",
-    wrong3: "Fabuland",
-    image: "assets/images/duplo.png",
-    picked: false
-};
-
-var q19 = {
-    question: "What year was the Lego Movie released?",
-    right: "2014",
-    wrong1: "2015",
-    wrong2: "2013",
-    wrong3: "2012",
-    image: "assets/images/legomovie.png",
-    picked: false
-};
-
-var q20 = {
-    question: "Who voices Lego Batman in the Lego series of films?",
-    right: "Will Arnett",
-    wrong1: "Christian Bale",
-    wrong2: "Chris Pratt",
-    wrong3: "Adam West",
-    image: "assets/images/batman.png",
-    picked: false
-};
-
-var questionArray = [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14, q15, q16, q17, q18, q19, q20];
+//Creates an array of all questions
+var questionArray = Object.values(questionsObject);
 
 var audio = new Audio("assets/awesome.mp3");
 
